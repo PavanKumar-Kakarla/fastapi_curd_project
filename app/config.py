c3f60e6472128d04fb2e5b8e6d94a08bb6cf9d00
PAVAN KUMAR KAKARLA <pavankakarla9@gmail.com>
2024-10-11 19:26:07 +0530

    Database Configuration Changes.

--- app/config.py
+++ app/config.py
@@ -1,10 +1,19 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+# Read MongoDB URI and database name from environment variables
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "itemsDB")
 
 # MongoDB Connection
-client = MongoClient('mongodb://localhost:27017')
+client = MongoClient(MONGO_URI)
 
 # Creating the Database
-db = client['itemsDB']
+db = client[DATABASE_NAME]
 
 # Creating the Collections
 items_collection = db['Items']
