32101a3eff3e6a2f84fae8460358c3933235fd0d
PAVAN KUMAR KAKARLA <pavankakarla9@gmail.com>
2024-10-12 08:30:57 +0530

    Added Procfile.

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 
 app.include_router(items.router)
 app.include_router(clock_in.router)
+
+
+@app.get('/')
+async def welcome():
+    return {"message": "Hello welcome to FastAPI."} 
