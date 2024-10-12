c56783963c38d3f05392d106bf5699d1c5477b46
PAVAN KUMAR KAKARLA <pavankakarla9@gmail.com>
2024-10-12 11:12:00 +0530

    Fixed Heroku crash.

--- app/main.py
+++ app/main.py
@@ -11,3 +11,9 @@ app.include_router(clock_in.router)
 @app.get('/')
 async def welcome():
     return {"message": "Hello welcome to FastAPI."} 
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
